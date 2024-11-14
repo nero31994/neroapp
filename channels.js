d11058500ce31d7ccaa49547c1230e348708425c
nero31994 <arenaofvalorph937@gmail.com>
2024-11-14 14:32:07 +0800

    channels.js

--- channels.js
+++ channels.js
@@ -15,14 +15,14 @@ const channels = {
 		type: 'mpd'
     },
     Animax: {
-        name: 'Animax',
+        name: 'ANIMAX',
         url: 'https://qp-pldt-live-grp-07-prod.akamaized.net/out/u/cg_animax_sd.mpd',
         keyId: '92032b0e41a543fb9830751273b8debd',
         key: '03f8b65e2af785b10d6634735dbe6c11',
 		type: 'mpd'
     },
     nickelodeon: {
-        name: 'Nickelodeon',
+        name: 'NICKELODEON',
         url: 'https://qp-pldt-live-grp-11-prod.akamaized.net/out/u/dr_nickelodeon.mpd',
         keyId: '9ce58f37576b416381b6514a809bfd8b',
         key: 'f0fbb758cdeeaddfa3eae538856b4d72',
@@ -36,7 +36,7 @@ const channels = {
 		type: 'mpd'
     },
     bilyonaryoch: {
-        name: 'Bilyonaryo',
+        name: 'BILYONARYO',
         url: 'https://qp-pldt-live-grp-05-prod.akamaized.net/out/u/bilyonaryoch.mpd',
         keyId: '227ffaf09bec4a889e0e0988704d52a2',
         key: 'b2d0dce5c486891997c1c92ddaca2cd2',
@@ -50,56 +50,56 @@ const channels = {
 		type: 'mpd'
     },
     truefmtv: {
-        name: 'True FM TV',
+        name: 'TRUE FM TV',
         url: 'https://qp-pldt-live-grp-08-prod.akamaized.net/out/u/truefm_tv.mpd',
         keyId: '0559c95496d44fadb94105b9176c3579',
         key: '40d8bb2a46ffd03540e0c6210ece57ce',
 		type: 'mpd'
     },
     arirang: {
-        name: 'Arirang',
+        name: 'ARIRANG',
         url: 'https://qp-pldt-live-grp-01-prod.akamaized.net/out/u/arirang_sd.mpd',
         keyId: '13815d0fa026441ea7662b0c9de00bcf',
         key: '2d99a55743677c3879a068dd9c92f824',
 		type: 'mpd'
     },
     onenews_hd: {
-        name: 'One News HD',
+        name: 'ONE NEWS HD',
         url: 'https://qp-pldt-live-grp-04-prod.akamaized.net/out/u/onenews_hd1.mpd',
         keyId: 'd39eb201ae494a0b98583df4d110e8dd',
         key: '6797066880d344422abd3f5eda41f45f',
 		type: 'mpd'
     },
     oneph: {
-        name: 'One PH',
+        name: 'ONE PH',
         url: 'https://qp-pldt-live-grp-04-prod.akamaized.net/out/u/oneph_sd.mpd',
         keyId: '92834ab4a7e1499b90886c5d49220e46',
         key: 'a7108d9a6cfcc1b7939eb111daf09ab3',
 		type: 'mpd'
     },
     onesports: {
-        name: 'One Sports',
+        name: 'ONE SPORTS HD',
         url: 'https://qp-pldt-live-grp-07-prod.akamaized.net/out/u/cg_onesports_hd.mpd',
         keyId: '53c3bf2eba574f639aa21f2d4409ff11',
         key: '3de28411cf08a64ea935b9578f6d0edd',
 		type: 'mpd'
     },
     uaap_varsity: {
-        name: 'UAAP Varsity',
+        name: 'UAAP VARSITY',
         url: 'https://qp-pldt-live-grp-04-prod.akamaized.net/out/u/cg_uaap_cplay_sd.mpd',
         keyId: '95588338ee37423e99358a6d431324b9',
         key: '6e0f50a12f36599a55073868f814e81e',
 		type: 'mpd'
     },
     premiersports_hd: {
-        name: 'Premier Sports',
+        name: 'PREMIER SPORTS',
         url: 'https://qp-pldt-live-grp-03-prod.akamaized.net/out/u/cg_premiersports_hd1.mpd',
         keyId: 'fc19c98cb9504a0fb78b22fea0a4b814',
         key: 'ea683112a96d4ae6c32d4ea13923e8c7',
 		type: 'mpd'
     },
     premier_tennishd: {
-        name: 'Premier Tennis',
+        name: 'PREMIER TENNIS',
         url: 'https://qp-pldt-live-grp-13-prod.akamaized.net/out/u/dr_premiertennishd.mpd',
         keyId: '59454adb530b4e0784eae62735f9d850',
         key: '61100d0b8c4dd13e4eb8b4851ba192cc',
@@ -120,68 +120,68 @@ const channels = {
      	type: 'mpd'
     },
     tapactionflix_hd: {
-        name: 'Tap Action Flix',
+        name: 'TAP ACTION FLIX',
         url: 'https://qp-pldt-live-grp-06-prod.akamaized.net/out/u/cg_tapactionflix_hd1.mpd',
         keyId: 'bee1066160c0424696d9bf99ca0645e3',
         key: 'f5b72bf3b89b9848de5616f37de040b7',
 	    type: 'mpd'
     },
     thrill: {
-       name: 'Thrill',
+       name: 'THRILL',
         url: 'https://qp-pldt-live-grp-06-prod.akamaized.net/out/u/cg_thrill_sd.mpd',
         keyId: '928114ffb2394d14b5585258f70ed183',
         key: 'a82edc340bc73447bac16cdfed0a4c62',
 		type: 'mpd'
     },
 	hits_now: {
-        name: 'Hits Now',
+        name: 'HITS NOW',
         url: 'https://qp-pldt-live-grp-09-prod.akamaized.net/out/u/cg_hitsnow.mpd',
         keyId: '14439a1b7afc4527bb0ebc51cf11cbc1',
         key: '92b0287c7042f271b266cc11ab7541f1',
 		type: 'mpd'
     },
 	hbo_family: {
-        name: 'HBO Family',
+        name: 'HBO FAMILY',
         url: 'https://qp-pldt-live-grp-03-prod.akamaized.net/out/u/cg_hbofam.mpd',
         keyId: '872910c843294319800d85f9a0940607',
         key: 'f79fd895b79c590708cf5e8b5c6263be',
 		type: 'mpd'
     },
 	cinemax: {
-        name: 'Cinemax',
+        name: 'CINEMAX',
         url: 'https://qp-pldt-live-grp-01-prod.akamaized.net/out/u/cg_cinemax.mpd',
         keyId: 'b207c44332844523a3a3b0469e5652d7',
         key: 'fe71aea346db08f8c6fbf0592209f955',
 		type: 'mpd'
     },
 	hbo_signature: {
-        name: 'HBO Signature',
+        name: 'HBO SIGNATURE',
         url: 'https://qp-pldt-live-grp-01-prod.akamaized.net/out/u/cg_hbosign.mpd',
         keyId: 'a06ca6c275744151895762e0346380f5',
         key: '559da1b63eec77b5a942018f14d3f56f',
 		type: 'mpd'
     },
 	hbo_hits: {
-    name: 'HBO Hits',
+    name: 'HBO HITS',
         url: 'https://qp-pldt-live-grp-09-prod.akamaized.net/out/u/cg_hbohits.mpd',
         keyId: 'b04ae8017b5b4601a5a0c9060f6d5b7d',
         key: 'a8795f3bdb8a4778b7e888ee484cc7a1',
 		type: 'mpd'
     },
         kmovies: {
-        name: 'K-Movies',
+        name: 'K-MOVIES',
         url: 'https://7732c5436342497882363a8cd14ceff4.mediatailor.us-east-1.amazonaws.com/v1/master/04fd913bb278d8775298c26fdca9d9841f37601f/Plex_NewMovies/playlist.m3u8',
         type: 'hls'
     },
 	tvn_movies: {
-    name: 'TVN Movies',
+    name: 'TVN MOVIES',
         url: 'https://qp-pldt-live-grp-07-prod.akamaized.net/out/u/cg_tvnmovie.mpd',
         keyId: '2e53f8d8a5e94bca8f9a1e16ce67df33',
         key: '3471b2464b5c7b033a03bb8307d9fa35',
 		type: 'mpd'
     },
 	dreamworks_tag: {
-    name: 'Dreamworks Tagalog',
+    name: 'DREAMWORKS TAGALOG',
         url: 'https://qp-pldt-live-grp-07-prod.akamaized.net/out/u/cg_dreamworktag.mpd',
         keyId: '564b3b1c781043c19242c66e348699c5',
         key: 'd3ad27d7fe1f14fb1a2cd5688549fbab',
@@ -195,7 +195,7 @@ const channels = {
 		type: 'mpd'
     },
 	popup_ppv_hd: {
-    name: 'Popup PPV HD',
+    name: 'POPUP PPV HD',
         url: 'https://qp-pldt-live-grp-13-prod.akamaized.net/out/u/dr_popupppvhd.mpd',
         keyId: '286e1c2d622c4c73b5c3d72e4a848abd',
         key: 'b7fad67599c1ce3c2fbc9d02b901be05',
@@ -216,7 +216,7 @@ const channels = {
 		type: 'mpd'
     },
 	dreamworks_hd: {
-    name: 'Dreamworks HD',
+    name: 'DREAMWORKS HD',
         url: 'https://qp-pldt-live-grp-02-prod.akamaized.net/out/u/cg_dreamworks_hd1.mpd',
         keyId: '4ab9645a2a0a47edbd65e8479c2b9669',
         key: '8cb209f1828431ce9b50b593d1f44079',
@@ -230,21 +230,21 @@ const channels = {
 		type: 'mpd'
     },
 	tap_edge: {
-    name: 'TAP Edge',
+    name: 'TAP EDGE',
         url: 'https://qp-pldt-live-grp-12-prod.akamaized.net/out/u/dr_tapedge.mpd',
         keyId: '0f854034412b11edb8780242ac120002',
         key: 'af8ad1c5e3f2e1b751a4f64608f24275',
 		type: 'mpd'
     },
 	tap_sports: {
-    name: 'TAP Sports',
+    name: 'TAP SPORTS',
         url: 'https://qp-pldt-live-grp-11-prod.akamaized.net/out/u/dr_tapsports.mpd',
         keyId: 'eabd2d95c89e42f2b0b0b40ce4179ea0',
         key: '0e7e35a07e2c12822316c0dc4873903f',
 		type: 'mpd'
     },
 	one_sports_plus_hd: {
-    name: 'One Sports Plus HD',
+    name: 'ONE SPORTS PLUS HD',
         url: 'https://qp-pldt-live-grp-03-prod.akamaized.net/out/u/cg_onesportsplus_hd1.mpd',
         keyId: '322d06e9326f4753a7ec0908030c13d8',
         key: '1e3e0ca32d421fbfec86feced0efefda',
@@ -258,7 +258,7 @@ const channels = {
 		type: 'mpd'
     },
 	lifetime: {
-    name: 'Lifetime',
+    name: 'LIFETIME',
         url: 'https://qp-pldt-live-grp-11-prod.akamaized.net/out/u/dr_lifetime.mpd',
         keyId: 'cf861d26e7834166807c324d57df5119',
         key: '64a81e30f6e5b7547e3516bbf8c647d0',
@@ -272,40 +272,40 @@ const channels = {
 		type: 'mpd'
     },
 	aljazeera: {
-    name: 'Al Jazeera',
+    name: 'Al JAZEERA ',
         url: 'https://live-hls-web-aje.getaj.net/AJE/index.m3u8',
         type: 'hls'
     },
 	foodnetwork_hd: {
-    name: 'Food Network HD',
+    name: 'FOOD NETWORK HD',
         url: 'https://qp-pldt-live-grp-09-prod.akamaized.net/out/u/cg_foodnetwork_hd1.mpd',
         keyId: 'b7299ea0af8945479cd2f287ee7d530e',
         key: 'b8ae7679cf18e7261303313b18ba7a14',
 		type: 'mpd'
     },
 	history_hd: {
-    name: 'History HD',
+    name: 'HISTORY HD',
         url: 'https://qp-pldt-live-grp-11-prod.akamaized.net/out/u/dr_historyhd.mpd',
         keyId: 'a7724b7ca2604c33bb2e963a0319968a',
         key: '6f97e3e2eb2bade626e0281ec01d3675',
 		type: 'mpd'
     },
 	fashiontv_hd: {
-    name: 'Fashion TV HD',
+    name: 'FASHION TV HD',
         url: 'https://qp-pldt-live-grp-11-prod.akamaized.net/out/u/dr_fashiontvhd.mpd',
         keyId: '971ebbe2d887476398e97c37e0c5c591',
         key: '472aa631b1e671070a4bf198f43da0c7',
 		type: 'mpd'
     },
 	bloomberg: {
-    name: 'Bloomberg',
+    name: 'BLOOMBERG',
         url: 'https://qp-pldt-live-grp-09-prod.akamaized.net/out/u/bloomberg_sd.mpd',
         keyId: 'ef7d9dcfb99b406cb79fb9f675cba426',
         key: 'b24094f6ca136af25600e44df5987af4',
 		type: 'mpd'
     },
 	depedchannel: {
-    name: 'DepEd Channel',
+    name: 'DEP ED CHANNEL',
         url: 'https://qp-pldt-live-grp-07-prod.akamaized.net/out/u/depedch_sd.mpd',
         keyId: '0f853706412b11edb8780242ac120002',
         key: '2157d6529d80a760f60a8b5350dbc4df',
@@ -326,7 +326,7 @@ const channels = {
 		type: 'mpd'
     },
 	knowledge: {
-    name: 'Knowledge Channel',
+    name: 'KNOWLEDGE CHANNEL',
         url: 'https://qp-pldt-live-grp-13-prod.akamaized.net/out/u/dr_knowledgechannel.mpd',
         keyId: '0f856fa0412b11edb8780242ac120002',
         key: '783374273ef97ad3bc992c1d63e091e7',
@@ -340,7 +340,7 @@ const channels = {
 		type: 'mpd'
     },
 	channelnews_asia: {
-    name: 'Channel News Asia',
+    name: 'CHANNEL NEWS ASIA',
         url: 'https://qp-pldt-live-grp-13-prod.akamaized.net/out/u/dr_channelnewsasia.mpd',
         keyId: 'b259df9987364dd3b778aa5d42cb9acd',
         key: '753e3dba96ab467e468269e7e33fb813',
@@ -368,42 +368,42 @@ const channels = {
 		type: 'mpd'
     },
 	animal_planet: {
-    name: 'Animal Planet',
+    name: 'ANIMAL PLANET',
         url: 'https://qp-pldt-live-grp-02-prod.akamaized.net/out/u/cg_animal_planet_sd.mpd',
         keyId: '436b69f987924fcbbc06d40a69c2799a',
         key: 'c63d5b0d7e52335b61aeba4f6537d54d',
 		type: 'mpd'
     },
 	abc_australia: {
-    name: 'ABC Australia',
+    name: 'ABC AUSTRALIA',
         url: 'https://qp-pldt-live-grp-10-prod.akamaized.net/out/u/dr_abc_aus.mpd',
         keyId: '389497f9f8584a57b234e27e430e04b7',
         key: '3b85594c7f88604adf004e45c03511c0',
 		type: 'mpd'
     },
 	bbcearth_hd: {
-    name: 'BBC Earth',
+    name: 'BBC EARTH',
         url: 'https://qp-pldt-live-grp-03-prod.akamaized.net/out/u/cg_bbcearth_hd1.mpd',
         keyId: '34ce95b60c424e169619816c5181aded',
         key: '0e2a2117d705613542618f58bf26fc8e',
 		type: 'mpd'
     },
 	bbcworld_news: {
-    name: 'BBCWORLD News',
+    name: 'BBCWORLD NEWS',
         url: 'https://qp-pldt-live-grp-04-prod.akamaized.net/out/u/bbcworld_news_sd.mpd',
         keyId: 'f59650be475e4c34a844d4e2062f71f3',
         key: '119639e849ddee96c4cec2f2b6b09b40',
 		type: 'mpd'
     },
 	travel_channel: {
-    name: 'Travel Channel',
+    name: 'TRAVEL CHANNEL',
         url: 'https://qp-pldt-live-grp-08-prod.akamaized.net/out/u/travel_channel_sd.mpd',
         keyId: 'f3047fc13d454dacb6db4207ee79d3d3',
         key: 'bdbd38748f51fc26932e96c9a2020839',
 		type: 'mpd'
     },
 	asianfoodnetwork: {
-    name: 'Asian Food Network',
+    name: 'ASIAN FOOD NETWORK',
         url: 'https://qp-pldt-live-grp-07-prod.akamaized.net/out/u/asianfoodnetwork_sd.mpd',
         keyId: '1619db30b9ed42019abb760a0a3b5e7f',
         key: '5921e47fb290ae263291b851c0b4b6e4',
@@ -417,77 +417,77 @@ const channels = {
 		type: 'mpd'
     },
 	crime_investigation: {
-    name: 'Crime Investigation',
+    name: 'CRIME INVESTIGATION',
         url: 'https://qp-pldt-live-grp-12-prod.akamaized.net/out/u/dr_crime_invest.mpd',
         keyId: '21e2843b561c4248b8ea487986a16d33',
         key: 'db6bb638ccdfc1ad1a3e98d728486801',
 		type: 'mpd'
     },
 	tech_storm: {
-    name: 'Tech Storm',
+    name: 'TECH STORM',
         url: 'https://qp-pldt-live-grp-11-prod.akamaized.net/out/u/dr_tech_storm.mpd',
         keyId: '5675d85ce6754ba6aa8f6acc4660f76f',
         key: '140bfb365cf143c349f68699238a610c',
 		type: 'mpd'
     },
 	kix_hd: {
-    name: 'Kix HD',
+    name: 'KIX HD',
         url: 'https://qp-pldt-live-grp-06-prod.akamaized.net/out/u/kix_hd1.mpd',
         keyId: 'a8d5712967cd495ca80fdc425bc61d6b',
         key: 'f248c29525ed4c40cc39baeee9634735',
 		type: 'mpd'
     },
 	cartoon_net_hd: {
-    name: 'Cartoon Network HD',
+    name: 'CARTOON NETWORK HD',
         url: 'https://qp-pldt-live-grp-12-prod.akamaized.net/out/u/dr_cartoonnetworkhd.mpd',
         keyId: 'a2d1f552ff9541558b3296b5a932136b',
         key: 'cdd48fa884dc0c3a3f85aeebca13d444',
 		type: 'mpd'
     },
 	nickjr: {
-    name: 'Nickjr',
+    name: 'NICKJR',
         url: 'https://qp-pldt-live-grp-12-prod.akamaized.net/out/u/dr_nickjr.mpd',
         keyId: 'bab5c11178b646749fbae87962bf5113',
         key: '0ac679aad3b9d619ac39ad634ec76bc8',
 		type: 'mpd'
     },
 	nickelodeon: {
-    name: 'Nickelodeon',
+    name: 'NICKELODEON',
         url: 'https://qp-pldt-live-grp-11-prod.akamaized.net/out/u/dr_nickelodeon.mpd',
         keyId: '9ce58f37576b416381b6514a809bfd8b',
         key: 'f0fbb758cdeeaddfa3eae538856b4d72',
 		type: 'mpd'
     },
 	blueant_extreme: {
-    name: 'Rock Action',
+    name: 'ROCK ACTION',
         url: 'https://qp-pldt-live-grp-13-prod.akamaized.net/out/u/dr_rockextreme.mpd',
         keyId: '0f852fb8412b11edb8780242ac120002',
         key: '4cbc004d8c444f9f996db42059ce8178',
 		type: 'mpd'
     },
 	rock_entertainment: {
-    name: 'Rock Entertainment',
+    name: 'ROCK ENTERTAINMENT',
         url: 'https://qp-pldt-live-grp-13-prod.akamaized.net/out/u/dr_rockentertainment.mpd',
         keyId: 'e4ee0cf8ca9746f99af402ca6eed8dc7',
         key: 'be2a096403346bc1d0bb0f812822bb62',
 		type: 'mpd'
     },
 	discovery: {
-    name: 'Discovery',
+    name: 'DISCOVERY ',
         url: 'https://qp-pldt-live-grp-13-prod.akamaized.net/out/u/dr_discovery.mpd',
         keyId: 'd9ac48f5131641a789328257e778ad3a',
         key: 'b6e67c37239901980c6e37e0607ceee6',
 		type: 'mpd'
     },
 	kix_hd: {
-    name: 'Kix HD',
+    name: 'KIX HD',
         url: 'https://qp-pldt-live-grp-06-prod.akamaized.net/out/u/kix_hd1.mpd',
         keyId: 'a8d5712967cd495ca80fdc425bc61d6b',
         key: 'f248c29525ed4c40cc39baeee9634735',
 		type: 'mpd'
     },
 	kbs_world: {
-    name: 'KBS World',
+    name: 'KBS WORLD',
         url: 'https://qp-pldt-live-grp-12-prod.akamaized.net/out/u/dr_kbsworld.mpd',
         keyId: '22ff2347107e4871aa423bea9c2bd363',
         key: 'c6e7ba2f48b3a3b8269e8bc360e60404',
@@ -501,19 +501,19 @@ const channels = {
 		type: 'mpd'
     },
 	teleradyo: {
-    name: 'Teleradyo',
+    name: 'TELERADYO',
         url: 'https://cdn-ue1-prod.tsv2.amagi.tv/linear/amg01006-abs-cbn-teleradyo-dash-abscbnono/index.mpd',
         keyId: '47c093e0c9fd4f80839a0337da3dd876',
         key: '50547394045b3d047dc7d92f57b5fb33',
 		type: 'mpd'
 		 },
 	cinemaone: {
-        name: 'CinemaOne',
+        name: 'CINEMA ONE',
         url: 'https://cinemaone-abscbn-ono.amagi.tv/index.m3u8',
         type: 'hls'
          },	 
     cinemo: {
-        name: 'CineMo!',
+        name: 'CINE MO!',
         url: 'https://cinemo-abscbn-ono.amagi.tv/playlist.m3u8',
         type: 'hls'
          },
