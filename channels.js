0412656b18c42a9151068dbdc6396fbb04168547
nero31994 <arenaofvalorph937@gmail.com>
2025-01-26 10:20:20 +0800

    Update channels.js

--- channels.js
+++ channels.js
@@ -17,8 +17,8 @@ KAP: {
         type: 'hls'
          }, 
 KAPAMILYA_CHANNEL: {
-    name: 'KAPAMILYA CHANNEL',
-    url: 'http://115.146.176.131/live/TimRizal/TimRizal/89.m3u8',
+    name: 'JUST FOR LAUGH',
+    url: 'https://epg.provider.plex.tv/library/parts/5e20b730f2f8d5003d739db7-64704954e536e0faaf10be9f/variant.m3u8?x-plex-token=sE38z2Coa9z6BhtRqbPm&x-plex-advertising-identifier=&x-plex-advertising-donottrack=0&x-plex-drm=&x-plex-device-screen-resolution=&x-plex-device-vendor=&x-plex-version=&x-plex-session-id=&x-plex-playback-session-id=&x-plex-playback-id=p_9b287a21-cb74-4905-9920-cf78e8c152ce&x-plex-provider-streaming-start=1737857606&url=6883812ad10c39c69cb45a329fc009f5-7bd6f54c7d9982a1d715addcce6a30e2eb4ab46e05e3bde6374112dd6f24fe170b7432a2f76f373805d3c3fe658817e8ef7ac95894134989582236d7f344d2e1296f3ba13377bb38c88aeceaea2072d8846a61c0efc9a3068f77b1d67550ee00ff81bf74e5112be0790d7d6557a7afcf1a4aa8d5876739e0babcb3c56b90916f2d60860c043b5c0f4d5d9805471b3d46fbe7e2477164ad9fa9a77134fa532dd3cf5456e1099c7581008d555bb6e57e9d57707aac75a6c4e056145b99145d5627e44695ca1f66f5919c19edb53d7273a5',
     type: 'hls',
     
 },
