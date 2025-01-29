12003e155349fabea77306561455dae4c4abc14e
nero31994 <arenaofvalorph937@gmail.com>
2025-01-29 09:35:20 +0800

    Delete off-canvas-menu.js

--- off-canvas-menu.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const menu = document.getElementById("offCanvasMenu");
-    if (menu.style.left === "0px") {
-        menu.style.left = "-250px";
-    } else {
-        menu.style.left = "0px";
-    }
-}
