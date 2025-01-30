5a14c7a6a3b7dfd2659d381d2604204bb7c3b181
nero31994 <arenaofvalorph937@gmail.com>
2025-01-30 08:54:18 +0800

    Update channels.js

--- channels.js
+++ channels.js
@@ -1,12 +1,12 @@
 
 const channels = {
 
-       widevineAlpha: {
+  TEST: {
         name: 'TEST',
         url: 'http://143.44.136.110:6910/001/2/ch00000090990000001274/manifest.mpd?virtualDomain=001.live_hls.zte.com',
-        type: 'mpd',
-        url: 'http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00'
-	    },
+        url: 'http://143.44.136.74:9443/widevine/?deviceId=02:00:00:00:00:00',
+	 type: 'mpd' 
+         },
 GTV: {
         name: 'GTV',
         url: 'https://ott.studentsdemo.com/stream/phcathenatv/gtv/master.m3u8?u=phc&p=1ed3395bba4d481a74e631943df9643663da8f300a8edececcb8b7da46349f01',
